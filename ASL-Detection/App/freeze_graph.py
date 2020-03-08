a30f4d65805df5da8990c81193f155025461958a
Utkarsh Sharma <skhiearth@gmail.com>
2020-03-08 23:37:37 +0530

    model opt

--- ASL-Detection/App/freeze_graph.py
+++ ASL-Detection/App/freeze_graph.py
@@ -1,6 +1,4 @@
 import tensorflow as tf
-from tensorflow_core.python.framework import graph_io
-from App.model_opt import optimizeModel
 
 tf.compat.v1.Session()
 
@@ -17,5 +15,3 @@ model = tf.keras.models.load_model(model_fname)
 with tf.compat.v1.keras.backend.get_session() as sess:
     tf.compat.v1.saved_model.simple_save(sess, export_dir, inputs={"keys": model.input},
                                          outputs={t.name: t for t in model.outputs})
-
-# optimizeModel()
--- ASL-Detection/App/model_opt.py
+++ ASL-Detection/App/model_opt.py
@@ -1,12 +1,13 @@
-def optimizeModel():
-    mo_tf_path = '/opt/intel/openvino/deployment_tools/model_optimizer/mo_tf.py'
+mo_tf_path = '/opt/intel/openvino/deployment_tools/model_optimizer/mo_tf.py'
 
-    pb_file = './model/frozen_model.pb'
-    output_dir = './model'
-    img_height = 224
-    input_shape = [1, img_height, img_height, 3]
-    input_shape_str = str(input_shape).replace(' ', '')
-    input_shape_str
+pb_file = 'models/frozen/saved_model.pb'
+output_dir = 'models/optimized'
+img_height = 64
+input_shape = [1, img_height, img_height, 3]
+input_shape_str = str(input_shape).replace(' ', '')
 
-    exec(open("mo_tf_path --input_model pb_file --output_dir output_dir --input_shape input_shape_str --data_type FP16")
-         .read())
+print(input_shape_str)
+
+# Run the following in terminal:
+# python3 {mo_tf_path} --input_model {pb_file} --output_dir {output_dir} --input_shape {input_shape_str} --data_type
+# FP16
